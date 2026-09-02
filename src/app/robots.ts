5b9d7b903f87fd0fc9413e41ca87c5bf8901aa10
electronicmbtech-8998 <dellmmdell35@gmail.com>
2026-09-02 01:09:05 +0100

    fix: remove calculators from robots.txt disallow
    - Allow Google to crawl calculator pages
    - Critical for SEO indexing

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -19,7 +19,6 @@ export default function robots(): MetadataRoute.Robots {
           '/sign-in',
           '/sign-up',
           '/setup',
-          '/calculators/',
         ],
       },
       {
